bd149e7fd5328ac5c0de3bf3442e20da54ec1484
Tomi Kalmi <tomi@chartmetric.com>
2020-02-11 00:04:20 -0800

    Enhance metadata in NewsItem

--- src/components/NewsItem/NewsItem.js
+++ src/components/NewsItem/NewsItem.js
@@ -7,9 +7,21 @@ const NewsItem = ({ id }) => {
 
   // Fetch story
   const story = useSelector(state => state.firebase.data.v0?.item?.[id]);
-  // const story = {};
 
-  // return <span>Foobar</span>;
+  const getTimeDiff = time => {
+    const currentTime = new Date().getTime() / 1000;
+    const diff = currentTime - time;
+    if (diff < 60) {
+      return `${Math.round(diff)} seconds ago`;
+    } else if (diff < 3_600) {
+      return `${Math.round(diff / 60)} minutes ago`;
+    } else if (diff < 86_400) {
+      return `${Math.round(diff / 3_600)} hours ago`;
+    } else if (diff < 31_536_000) {
+      return `${Math.round(diff / 86_400)} days ago`;
+    }
+    return `${Math.round(diff / 31_536_000)} years ago`;
+  };
 
   if (!isLoaded(story)) {
     return 'Loading...';
@@ -17,9 +29,18 @@ const NewsItem = ({ id }) => {
 
   return (
     <article>
-      <h1>
-        <a href={story.url}>{story.title}</a>
-      </h1>
+      <header>
+        <h1>
+          <a href={story.url}>{story.title}</a>
+        </h1>
+      </header>
+      <footer>
+        {story.score} Points by <address>{story.by}</address>{' '}
+        <time dateTime={new Date(story.time).toString()}>
+          {getTimeDiff(story.time)}
+        </time>
+        | {story.descendants} comments
+      </footer>
     </article>
   );
 };
