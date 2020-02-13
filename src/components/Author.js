78a261fca9951acb1e8bd6d42ee3c92f6a87458d
Tomi Kalmi <tomi@chartmetric.com>
2020-02-12 22:28:03 -0800

    Add `label` prop to <Author>
    Show `author` as the default link text, but offer `label` prop to override it

--- src/components/Author.js
+++ src/components/Author.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Author = ({ author }) => (
+const Author = ({ author, label }) => (
   <address>
     <Link rel="author" to={`/user/${author}`}>
-      {author}
+      {label || author}
     </Link>
   </address>
 );
