c79356e82f6e63b6bed110848328ec5a071ba372
Tomi Kalmi <tomi@chartmetric.com>
2020-02-16 10:35:48 -0800

    Style <NewsItem> w/ a Reddit-like mobile layout

--- src/components/NewsItem/NewsItem.js
+++ src/components/NewsItem/NewsItem.js
@@ -2,12 +2,75 @@ import React from 'react';
 import { useFirebaseConnect, isLoaded } from 'react-redux-firebase';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 import Author from '../Author';
 import PublishTime from '../PublishTime';
 import { extractHostname } from '../../utils';
 
-const NewsItem = ({ id }) => {
+const Article = styled.article`
+  background: #feffff;
+
+  box-shadow: 20px 20px 60px #d8d9d9, -20px -20px 60px #ffffff;
+
+  margin-bottom: 10px;
+  padding: 5px 20px 15px;
+`;
+
+const Header = styled.header`
+  align-items: center;
+  display: flex;
+  flex-wrap: nowrap;
+  font-size: ${props => props.theme.smallFontSize};
+  line-height: 1.3;
+  margin-bottom: 8px;
+
+  .hostname {
+    font-size: ${props => props.theme.normalFontSize};
+  }
+`;
+
+const Index = styled.span`
+  align-items: center;
+  border: 1px solid gray;
+  border-radius: 50%;
+  display: flex;
+  height: 34px;
+  justify-content: center;
+  margin-right: 8px;
+  width: 34px;
+`;
+
+const Heading = styled.h1`
+  font-size: ${props => props.theme.normalFontSize};
+  line-height: 1.5;
+`;
+
+const Footer = styled.footer`
+  align-items: baseline;
+  display: flex;
+  font-size: ${props => props.theme.normalFontSize};
+  justify-content: space-between;
+`;
+
+const Separator = styled.span`
+  padding: 0 8px;
+  position: relative;
+
+  &::after {
+    background: gray;
+    border-radius: 50%;
+    content: '';
+    height: 4px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 4px;
+  }
+`;
+
+const NewsItem = ({ id, idx }) => {
   useFirebaseConnect(`v0/item/${id}`);
 
   // Fetch story
@@ -22,22 +85,28 @@ const NewsItem = ({ id }) => {
   }
 
   return (
-    <article>
-      <header>
-        <h1>
+    <Article>
+      <Header>
+        <Index>{idx}</Index>
+        <div>
+          <div className="hostname">{extractHostname(story.url)}</div>
+          <div>
+            Posted by <Author author={story.by} />
+            <Separator />
+            <PublishTime time={story.time} />
+          </div>
+        </div>
+      </Header>
+      <main>
+        <Heading>
           <a href={story.url || `/item/${id}`}>{story.title}</a>
-          {extractHostname(story.url) && (
-            <small>({extractHostname(story.url)})</small>
-          )}
-        </h1>
-      </header>
-      <footer>
-        {story.score} Points by
-        <Author author={story.by} />
-        <PublishTime time={story.time} /> |
+        </Heading>
+      </main>
+      <Footer>
+        <span>{story.score} Points</span>
         <Link to={`/item/${id}`}>{story.descendants} comments</Link>
-      </footer>
-    </article>
+      </Footer>
+    </Article>
   );
 };
 
