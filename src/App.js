789e328fb3033509cdc91be5d84a0972f6c08387
Tomi Kalmi <tomi@chartmetric.com>
2020-02-16 10:34:15 -0800

    Add theme and <ThemeProvider> to App

--- src/App.js
+++ src/App.js
@@ -2,36 +2,40 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import { store, reactReduxFirebaseProps } from './store.js';
 import News from './components/News/News.js';
 import ItemDetails from './components/ItemDetails/ItemDetails.js';
 import UserDetails from './components/UserDetails/UserDetails.js';
 import Header from './components/Header/Header';
+import { standardTheme } from './theme.js';
 
 function App() {
   return (
     <Provider store={store}>
       <ReactReduxFirebaseProvider {...reactReduxFirebaseProps}>
         <BrowserRouter>
-          <Header />
-          <Switch>
-            <Route path="/item/:itemId">
-              <ItemDetails />
-            </Route>
-            <Route path="/user/:userId">
-              <UserDetails />
-            </Route>
-            <Route path="/newstories">
-              <News />
-            </Route>
-            <Route path="/beststories">
-              <News />
-            </Route>
-            <Route path="/">
-              <News />
-            </Route>
-          </Switch>
+          <ThemeProvider theme={standardTheme}>
+            <Header />
+            <Switch>
+              <Route path="/item/:itemId">
+                <ItemDetails />
+              </Route>
+              <Route path="/user/:userId">
+                <UserDetails />
+              </Route>
+              <Route path="/newstories">
+                <News />
+              </Route>
+              <Route path="/beststories">
+                <News />
+              </Route>
+              <Route path="/">
+                <News />
+              </Route>
+            </Switch>
+          </ThemeProvider>
         </BrowserRouter>
       </ReactReduxFirebaseProvider>
     </Provider>
---
+++ src/theme.js
@@ -0,0 +1,9 @@
+const typography = {
+  smallFontSize: '12px',
+  normalFontSize: '16px',
+  largeFontSize: '18px'
+};
+
+export const standardTheme = {
+  ...typography
+};
