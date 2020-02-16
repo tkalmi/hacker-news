630d2186be33f305a2c1c6bbe7978f2fccdfdf4a
Tomi Kalmi <tomi@chartmetric.com>
2020-02-16 10:32:47 -0800

    Fix dateTime attr in <PublishTime>
    HN API returns Unix time in seconds, but Date API needs milliseconds so multiply by 1,000

--- src/components/PublishTime.js
+++ src/components/PublishTime.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { getTimeDiff } from '../utils';
 
 const PublishTime = ({ time }) => (
-  <time dateTime={new Date(time).toString()}>{getTimeDiff(time)}</time>
+  <time dateTime={new Date(time * 1_000).toString()}>{getTimeDiff(time)}</time>
 );
 
 export default PublishTime;
