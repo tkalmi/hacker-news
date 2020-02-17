52aa47f9f28b8958a0c2e8183c5410371dfc196c
Tomi Kalmi <tomi@chartmetric.com>
2020-02-16 19:57:02 -0800

    Style <Comment>

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -2,10 +2,38 @@ import React, { useState } from 'react';
 import { useFirebaseConnect, isLoaded } from 'react-redux-firebase';
 import { useSelector } from 'react-redux';
 import sanitizeHtml from 'sanitize-html';
+import styled from 'styled-components';
 
 import Author from '../Author';
 import { addBlockQuotes, ensureHonestLinks } from '../../utils';
 import PublishTime from '../PublishTime';
+import CommentList from './CommentList';
+import Separator from '../Separator';
+
+const COMMENT_COLORS = ['lime', 'red', 'orange', 'yellow'];
+
+const CommentContainer = styled.article`
+  border-top: 1px solid lightgray;
+  padding-top: 10px;
+`;
+
+const CommentDetails = styled.div`
+  position: relative;
+
+  &::before {
+    background-color: ${props =>
+      props.depth > 0
+        ? COMMENT_COLORS[props.depth % COMMENT_COLORS.length]
+        : 'opaque'};
+    border-radius: 2px;
+    content: '';
+    height: 100%;
+    left: -10px;
+    position: absolute;
+    top: 0;
+    width: 3px;
+  }
+`;
 
 const Comment = ({ depth = 0, id, originalPoster }) => {
   const [showMore, setShowMore] = useState(depth < 2);
@@ -28,31 +56,34 @@ const Comment = ({ depth = 0, id, originalPoster }) => {
   }
 
   return (
-    <article>
-      <header>
-        <Author author={comment.by}>
-          {comment.by === originalPoster && `${comment.by} [OP]`}
-        </Author>
-        | <PublishTime time={comment.time} />
-        {comment.kids?.length && showMore && (
-          <button onClick={toggleCollapse}>
-            {isCollapsed ? '+ Show Thread' : '- Collapse Thread'}
-          </button>
-        )}
-      </header>
-      <p
-        dangerouslySetInnerHTML={{
-          __html: comment.deleted
-            ? '[DELETED]'
-            : addBlockQuotes(ensureHonestLinks(sanitizeHtml(comment.text)))
-        }}
-      ></p>
+    <CommentContainer>
+      <CommentDetails depth={depth}>
+        <header>
+          <Author author={comment.by}>
+            {comment.by === originalPoster && `${comment.by} [OP]`}
+          </Author>
+          <Separator />
+          <PublishTime time={comment.time} />
+          {comment.kids?.length && showMore && (
+            <button onClick={toggleCollapse}>
+              {isCollapsed ? '+ Show Thread' : '- Collapse Thread'}
+            </button>
+          )}
+        </header>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: comment.deleted
+              ? '[DELETED]'
+              : addBlockQuotes(ensureHonestLinks(sanitizeHtml(comment.text)))
+          }}
+        ></p>
+      </CommentDetails>
 
       {comment.kids?.length &&
         (!showMore ? (
           <button onClick={handleShowMore}>More</button>
         ) : (
-          <ul style={isCollapsed ? { display: 'none' } : {}}>
+          <CommentList style={isCollapsed ? { display: 'none' } : {}}>
             {comment.kids.map(kid => (
               <li key={kid}>
                 <Comment
@@ -62,9 +93,9 @@ const Comment = ({ depth = 0, id, originalPoster }) => {
                 />
               </li>
             ))}
-          </ul>
+          </CommentList>
         ))}
-    </article>
+    </CommentContainer>
   );
 };
 
