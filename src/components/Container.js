f25ce84fa482d20b860d7396c1f45bbaa518bfd3
Tomi Kalmi <tomi@chartmetric.com>
2020-02-16 19:54:35 -0800

    Improve <Container> style

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   margin: auto;
-  max-width: 900px;
+  max-width: 800px;
+  padding: 0 10px;
 `;
 
 export default Container;
