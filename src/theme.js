c23659b5742cadb024b23596880dca92030778b4
Tomi Kalmi <tomi@chartmetric.com>
2020-02-17 20:45:57 -0800

    Add colors to theme

--- src/theme.js
+++ src/theme.js
@@ -4,6 +4,21 @@ const typography = {
   largeFontSize: '18px'
 };
 
+const colors = {
+  accentColor: 'lime',
+  cardBgColor: '#feffff',
+  darkFontColor: '#222',
+  lightAccentColor: 'aliceblue',
+  linkColor: 'blue',
+  linkVisitedColor: 'purple',
+  mediumFontColor: '#515151',
+  githubColor: 'black',
+  lightBgColor: '#eee',
+  lightFontColor: '#666',
+  secondaryAccentColor: 'orange'
+};
+
 export const standardTheme = {
+  ...colors,
   ...typography
 };
